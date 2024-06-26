f41d9789d244172faae5b895898c30abf1ff6c40
Stefan Sievert <stefan.sievert@stud-informatik.de>
2024-06-26 20:04:06 +0200

    fix: amend

--- module/data/actor-character.mjs
+++ module/data/actor-character.mjs
@@ -1,6 +1,5 @@
 import TestSystemActorBase from "./base-actor.mjs";
 import { NumberField, SchemaField } from foundry.data.fields;
-import { TEST_SYSTEM } from '../helpers/config.mjs';
 
 export default class TestSystemCharacter extends TestSystemActorBase {
 
@@ -16,7 +15,7 @@ export default class TestSystemCharacter extends TestSystemActorBase {
     });
 
     // Iterate over ability names and create a new SchemaField for each.
-    schema.abilities = new fields.SchemaField(Object.keys(TEST_SYSTEM.abilities).reduce((obj, ability) => {
+    schema.abilities = new fields.SchemaField(Object.keys(CONFIG.TEST_SYSTEM.abilities).reduce((obj, ability) => {
       obj[ability] = new fields.SchemaField({
         value: new fields.NumberField({ ...requiredInteger, initial: 10, min: 0 }),
       });
@@ -43,7 +42,7 @@ export default class TestSystemCharacter extends TestSystemActorBase {
       // Calculate the modifier using d20 rules.
       this.abilities[key].mod = Math.floor((this.abilities[key].value - 10) / 2);
       // Handle ability label localization.
-      this.abilities[key].label = game.i18n.localize(TEST_SYSTEM.abilities[key]) ?? key;
+      this.abilities[key].label = game.i18n.localize(CONFIG.TEST_SYSTEM.abilities[key]) ?? key;
     }
   }
 
